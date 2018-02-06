89b66b1c813c5ae1b8718f9feae1fd753a313b45
Sven Hettwer <sven.hettwer@consol.de>
2018-02-06 14:25:13 +0100

    Added documentation

--- server/src/main/java/com/consol/citrus/db/server/rules/Rule.java
+++ server/src/main/java/com/consol/citrus/db/server/rules/Rule.java
@@ -8,70 +8,100 @@ import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
+ * This class represents a functional predicate (mapping rule) that is applicable on objects of type P, if
+ * the objects fulfills a certain boolean-valued precondition predicate. If the rule is applied, the object of type P
+ * will be mapped in an object of type R.
+ *
  * @author Christoph Deppisch
  */
+@SuppressWarnings("unused")
 public class Rule<P, R, T extends Rule> {
 
     /** Logger */
     private Logger log = LoggerFactory.getLogger(getClass());
 
+    /** The predicate that has to be fulfilled so that the rule can be applied */
     private final RuleMatcher<P> ruleMatcher;
+
+    /** The application logic of the rule transforming P -> R */
     private final RuleExecutor<P, R> ruleExecutor;
 
+    /** The amount of invocations */
     private AtomicInteger maxInvocation;
 
+    /** The self reference of this rule */
     private final T self;
 
     /**
-     * Default constructor using all matcher and null executor.
+     * Default constructor that accepts all objects and returns null.
      */
     public Rule() {
         this((predicate -> null));
     }
 
     /**
-     * Constructor using all matcher and executor.
+     * Constructor accepting all objects and a specified functional predicate.
      */
-    public Rule(RuleExecutor<P, R> ruleExecutor) {
+    public Rule(final RuleExecutor<P, R> ruleExecutor) {
         this(RuleMatcher.matchAll(), ruleExecutor);
     }
 
     /**
-     * Constructor using matcher and executor.
-     * @param ruleMatcher
-     * @param ruleExecutor
+     * Constructor accepting a precondition predicate and functional predicate.
+     * @param ruleMatcher The predicate that specifies the precondition of the rule
+     * @param ruleExecutor The functional predicate to apply to the given object
      */
-    public Rule(RuleMatcher<P> ruleMatcher, RuleExecutor<P, R> ruleExecutor) {
+    public Rule(final RuleMatcher<P> ruleMatcher, final RuleExecutor<P, R> ruleExecutor) {
         this.ruleMatcher = ruleMatcher;
         this.ruleExecutor = ruleExecutor;
         this.self = (T) this;
     }
 
-    public final boolean matches(P predicate) {
-        boolean matching = (maxInvocation == null || maxInvocation.get() != 0) && ruleMatcher.match(predicate);
+    /**
+     * Determines whether a given object fulfills the precondition predicate of this rule
+     * @param candidate The object to evaluate the precondition predicate on
+     * @return The result of the predicate evaluation
+     */
+    public final boolean matches(final P candidate) {
+        final boolean matching = (maxInvocation == null || maxInvocation.get() != 0) && ruleMatcher.match(candidate);
 
         if (matching) {
-            log.debug(String.format("Found matching rule for predicate '%s'", Optional.ofNullable(predicate).map(Object::toString).orElse("")));
+            log.debug(String.format("Found matching rule for candidate '%s'", Optional.ofNullable(candidate).map(Object::toString).orElse("")));
         } else {
-            log.trace(String.format("Rule mismatch for predicate '%s'", Optional.ofNullable(predicate).map(Object::toString).orElse("")));
+            log.trace(String.format("Rule mismatch for candidate '%s'", Optional.ofNullable(candidate).map(Object::toString).orElse("")));
         }
 
         return matching;
     }
 
-    public final R apply(P predicate) throws JdbcServerException {
+    /**
+     * Applies the rule on the given domain object and returns the mapped codomain object
+     * @param domainObject The object to be mapped
+     * @return The mapped object of the codomain
+     * @throws JdbcServerException In case of an error
+     */
+    public final R apply(final P domainObject) throws JdbcServerException {
         if (maxInvocation != null) {
             maxInvocation.decrementAndGet();
         }
 
-        return ruleExecutor.then(predicate);
+        return ruleExecutor.then(domainObject);
     }
 
-    public T times(int times) {
+    /**
+     * Specifies the maximum invocations possible for that rule
+     * @param times The maximum amount of invocations
+     * @return The modified rule
+     */
+    public T times(final int times) {
         this.maxInvocation = new AtomicInteger(times);
         return self;
     }
 
+    /**
+     * Specifies that the rule can be applied without a maximum invocation amount
+     * @return The modified rule
+     */
     public T anyTimes() {
         this.maxInvocation = null;
         return self;

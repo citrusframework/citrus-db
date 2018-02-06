10704353f1b852d6919eec05e2d37583f36cba57
Sven Hettwer <sven.hettwer@consol.de>
2018-02-06 13:28:56 +0100

    Added documentation

--- server/src/main/java/com/consol/citrus/db/server/rules/RuleExecutor.java
+++ server/src/main/java/com/consol/citrus/db/server/rules/RuleExecutor.java
@@ -19,9 +19,20 @@ package com.consol.citrus.db.server.rules;
 import com.consol.citrus.db.server.JdbcServerException;
 
 /**
+ * This interface describes the transformation of an object of type P
+ * into a object of type R by executing transformation rules
+ *
  * @author Christoph Deppisch
  */
 public interface RuleExecutor<P, R> {
 
-    R then(P predicate) throws JdbcServerException;
+    /**
+     * Transforms an object of the domain type P into an object
+     * of the codomain R by executing the underlying implementation.
+     *
+     * @param domainElement The domain element to be transformed
+     * @return The transformed element of the codomain
+     * @throws JdbcServerException In case of an transformation error
+     */
+    R then(P domainElement) throws JdbcServerException;
 }

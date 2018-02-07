cdb02c826a067c7d97387e7736f6685871cd32e8
Sven Hettwer <sven.hettwer@consol.de>
2018-02-07 09:36:59 +0100

    Added AbstractRuleBuilderTest

---
+++ server/src/test/java/com/consol/citrus/db/server/builder/AbstractRuleBuilderTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2006-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.consol.citrus.db.server.builder;
+
+import com.consol.citrus.db.server.JdbcServerException;
+import com.consol.citrus.db.server.controller.RuleBasedController;
+import com.consol.citrus.db.server.rules.CloseConnectionRule;
+import com.consol.citrus.db.server.rules.Mapping;
+import com.consol.citrus.db.server.rules.Precondition;
+import org.testng.annotations.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.testng.Assert.assertEquals;
+
+public class AbstractRuleBuilderTest {
+
+    private RuleBasedController ruleBasedControllerMock = mock(RuleBasedController.class);
+
+    private AbstractRuleBuilder<CloseConnectionRule, Void, Boolean> builder =
+            new AbstractRuleBuilder<CloseConnectionRule, Void, Boolean>(ruleBasedControllerMock) {
+        @Override
+        protected CloseConnectionRule createRule(
+                final Precondition<Void> precondition,
+                final Mapping<Void, Boolean> mapping) {
+            return new CloseConnectionRule(precondition, mapping);
+        }
+    };
+
+    @Test
+    public void testConstructor(){
+
+        //GIVEN
+
+        //WHEN
+        final RuleBasedController controller = builder.getController();
+
+        //THEN
+        assertEquals(controller, ruleBasedControllerMock);
+    }
+
+    @Test(expectedExceptions = JdbcServerException.class)
+    public void testThenThrow(){
+
+        //GIVEN
+        final JdbcServerException jdbcServerException = new JdbcServerException();
+
+        //WHEN
+        final CloseConnectionRule closeConnectionRule = builder.thenThrow(jdbcServerException);
+
+        //THEN
+        verify(ruleBasedControllerMock).add(closeConnectionRule);
+        closeConnectionRule.applyOn(null);
+    }
+}

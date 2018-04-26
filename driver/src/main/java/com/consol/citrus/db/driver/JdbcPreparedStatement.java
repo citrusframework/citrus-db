97de68477d311da60686f6fc5524a285ef0111d6
Sven Hettwer <sven.hettwer@consol.de>
2018-04-26 16:56:08 +0200

    (#15) Overwrite values instead of enlarging the list

--- driver/src/main/java/com/consol/citrus/db/driver/JdbcPreparedStatement.java
+++ driver/src/main/java/com/consol/citrus/db/driver/JdbcPreparedStatement.java
@@ -67,87 +67,87 @@ public class JdbcPreparedStatement extends JdbcStatement implements PreparedStat
 
     @Override
     public void setNull(final int parameterIndex, final int sqlType) throws SQLException {
-        parameters.add(parameterIndex, null);
+        setParameter(parameterIndex, null);
     }
 
     @Override
     public void setBoolean(final int parameterIndex, final boolean x) throws SQLException {
-        parameters.add(parameterIndex - 1, x);
+        setParameter(parameterIndex, x);
     }
 
     @Override
     public void setByte(final int parameterIndex, final byte x) throws SQLException {
-        parameters.add(parameterIndex - 1, x);
+        setParameter(parameterIndex, x);
     }
 
     @Override
     public void setShort(final int parameterIndex, final short x) throws SQLException {
-        parameters.add(parameterIndex - 1, x);
+        setParameter(parameterIndex, x);
     }
 
     @Override
     public void setInt(final int parameterIndex, final int x) throws SQLException {
-        parameters.add(parameterIndex - 1, x);
+        setParameter(parameterIndex, x);
     }
 
     @Override
     public void setLong(final int parameterIndex, final long x) throws SQLException {
-        parameters.add(parameterIndex - 1, x);
+        setParameter(parameterIndex, x);
     }
 
     @Override
     public void setFloat(final int parameterIndex, final float x) throws SQLException {
-        parameters.add(parameterIndex - 1, x);
+        setParameter(parameterIndex, x);
     }
 
     @Override
     public void setDouble(final int parameterIndex, final double x) throws SQLException {
-        parameters.add(parameterIndex - 1, x);
+        setParameter(parameterIndex, x);
     }
 
     @Override
     public void setBigDecimal(final int parameterIndex, final BigDecimal x) throws SQLException {
-        parameters.add(parameterIndex - 1, x);
+        setParameter(parameterIndex, x);
     }
 
     @Override
     public void setString(final int parameterIndex, final String x) throws SQLException {
-        parameters.add(parameterIndex - 1, x);
+        setParameter(parameterIndex, x);
     }
 
     @Override
     public void setBytes(final int parameterIndex, final byte[] x) throws SQLException {
-        parameters.add(parameterIndex - 1, x);
+        setParameter(parameterIndex, x);
     }
 
     @Override
     public void setDate(final int parameterIndex, final Date x) throws SQLException {
-        parameters.add(parameterIndex - 1, x);
+        setParameter(parameterIndex, x);
     }
 
     @Override
     public void setTime(final int parameterIndex, final Time x) throws SQLException {
-        parameters.add(parameterIndex - 1, x);
+        setParameter(parameterIndex, x);
     }
 
     @Override
     public void setTimestamp(final int parameterIndex, final Timestamp x) throws SQLException {
-        parameters.add(parameterIndex - 1, x);
+        setParameter(parameterIndex, x);
     }
 
     @Override
     public void setAsciiStream(final int parameterIndex, final InputStream x, final int length) throws SQLException {
-        parameters.add(parameterIndex - 1, x);
+        setParameter(parameterIndex, x);
     }
 
     @Override
     public void setUnicodeStream(final int parameterIndex, final InputStream x, final int length) throws SQLException {
-        parameters.add(parameterIndex - 1, x);
+        setParameter(parameterIndex, x);
     }
 
     @Override
     public void setBinaryStream(final int parameterIndex, final InputStream x, final int length) throws SQLException {
-        parameters.add(parameterIndex - 1, x);
+        setParameter(parameterIndex, x);
     }
 
     @Override
@@ -157,12 +157,12 @@ public class JdbcPreparedStatement extends JdbcStatement implements PreparedStat
 
     @Override
     public void setObject(final int parameterIndex, final Object x, final int targetSqlType) throws SQLException {
-        parameters.add(parameterIndex - 1, x);
+        setParameter(parameterIndex, x);
     }
 
     @Override
     public void setObject(final int parameterIndex, final Object x) throws SQLException {
-        parameters.add(parameterIndex - 1, x);
+        setParameter(parameterIndex, x);
     }
 
     @Override
@@ -206,27 +206,27 @@ public class JdbcPreparedStatement extends JdbcStatement implements PreparedStat
 
     @Override
     public void setDate(final int parameterIndex, final Date x, final Calendar cal) throws SQLException {
-        parameters.add(parameterIndex - 1, x.toString());
+        setParameter(parameterIndex, x.toString());
     }
 
     @Override
     public void setTime(final int parameterIndex, final Time x, final Calendar cal) throws SQLException {
-        parameters.add(parameterIndex - 1, x.getTime());
+        setParameter(parameterIndex, x.getTime());
     }
 
     @Override
     public void setTimestamp(final int parameterIndex, final Timestamp x, final Calendar cal) throws SQLException {
-        parameters.add(parameterIndex - 1, x.getTime());
+        setParameter(parameterIndex, x.getTime());
     }
 
     @Override
     public void setNull(final int parameterIndex, final int sqlType, final String typeName) throws SQLException {
-        parameters.add(parameterIndex, null);
+        setParameter(parameterIndex, null);
     }
 
     @Override
     public void setURL(final int parameterIndex, final URL x) throws SQLException {
-        parameters.add(parameterIndex - 1, x);
+        setParameter(parameterIndex, x);
     }
 
     @Override
@@ -236,12 +236,12 @@ public class JdbcPreparedStatement extends JdbcStatement implements PreparedStat
 
     @Override
     public void setRowId(final int parameterIndex, final RowId x) throws SQLException {
-        parameters.add(parameterIndex - 1, x);
+        setParameter(parameterIndex, x);
     }
 
     @Override
     public void setNString(final int parameterIndex, final String x) throws SQLException {
-        parameters.add(parameterIndex - 1, x);
+        setParameter(parameterIndex, x);
     }
 
     @Override
@@ -276,7 +276,7 @@ public class JdbcPreparedStatement extends JdbcStatement implements PreparedStat
 
     @Override
     public void setObject(final int parameterIndex, final Object x, final int targetSqlType, final int scaleOrLength) throws SQLException {
-        parameters.add(parameterIndex - 1, x);
+        setParameter(parameterIndex, x);
     }
 
     @Override
@@ -329,6 +329,14 @@ public class JdbcPreparedStatement extends JdbcStatement implements PreparedStat
         throw new SQLException("Not supported JDBC prepared statement function 'setNClob'");
     }
 
+    void setParameter(final int parameterIndex, final Object value){
+        if (parameters.size() > parameterIndex) {
+            parameters.set(parameterIndex,value);
+        }else {
+            parameters.add(parameterIndex, value);
+        }
+    }
+
     private String composeStatement() {
         return preparedStatement + " - (" + parameters.stream().map(param -> param != null ? param.toString() : "null").collect(Collectors.joining(",")) + ")";
     }
@@ -337,3 +345,4 @@ public class JdbcPreparedStatement extends JdbcStatement implements PreparedStat
         return parameters;
     }
 }
+

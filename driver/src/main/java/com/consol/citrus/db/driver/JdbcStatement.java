61f1c34d14fede806d1371a84182f270c0b19e19
Can Pascal Yildiz <c.yildiz@cpyi.de>
2018-05-03 11:34:59 +0200

    Anpassung fur Tibco

--- driver/src/main/java/com/consol/citrus/db/driver/JdbcStatement.java
+++ driver/src/main/java/com/consol/citrus/db/driver/JdbcStatement.java
@@ -60,6 +60,10 @@ public class JdbcStatement implements Statement {
     public java.sql.ResultSet executeQuery(final String sqlQuery) throws SQLException {
         HttpResponse response = null;
         try {
+        	//Tibco Ping-Queries abfangen
+        	if (sqlQuery.equals("SELECT USER from DUAL")){
+        		return new JdbcResultSet(null, this);
+        	}
             response = httpClient.execute(RequestBuilder.post(serverUrl + "/query")
                     .setEntity(new StringEntity(sqlQuery, ContentType.create("text/plain", "UTF-8")))
                     .build());
@@ -203,7 +207,7 @@ public class JdbcStatement implements Statement {
 
     @Override
     public boolean getMoreResults() throws SQLException {
-        throw new SQLException("Not supported JDBC statement function 'getMoreResults'");
+        return false;
     }
 
     @Override

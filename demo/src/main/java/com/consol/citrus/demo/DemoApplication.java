4105fe31997be7217da7c8a58301488344995b9d
christophd <deppisch@consol.de>
2018-05-29 09:46:22 +0200

    Improved demo

--- demo/src/main/java/com/consol/citrus/demo/DemoApplication.java
+++ demo/src/main/java/com/consol/citrus/demo/DemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
-import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
@@ -16,9 +16,9 @@ public class DemoApplication {
 
     @Bean
     public DataSource dataSource(Environment environment) {
-        return new SingleConnectionDataSource(environment.getProperty("spring.datasource.url"),
+        return new DriverManagerDataSource(environment.getProperty("spring.datasource.url"),
                                                 environment.getProperty("spring.datasource.username"),
-                                                environment.getProperty("spring.datasource.password"), false);
+                                                environment.getProperty("spring.datasource.password"));
     }
 
     public static void main(String[] args) {

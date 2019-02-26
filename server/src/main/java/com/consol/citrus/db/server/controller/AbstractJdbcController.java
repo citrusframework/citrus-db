91324ac71e0f6342c7688ff7ee938210cf195236
Sven Hettwer <sven.hettwer@consol.de>
2019-02-26 14:43:15 +0100

    (citrusframework/citrus-db#36) Some cleanup -> reduced logging verbosity on info level

--- server/src/main/java/com/consol/citrus/db/server/controller/AbstractJdbcController.java
+++ server/src/main/java/com/consol/citrus/db/server/controller/AbstractJdbcController.java
@@ -17,7 +17,6 @@
 package com.consol.citrus.db.server.controller;
 
 import com.consol.citrus.db.driver.exchange.DatabaseResult;
-import com.consol.citrus.db.server.JdbcServerException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -43,114 +42,117 @@ public abstract class AbstractJdbcController implements JdbcController {
      * @param sql The sql statement to map to a DataSet
      * @return The data set mapped to the given query
      */
-    protected abstract DatabaseResult handleQuery(String sql) throws JdbcServerException;
+    protected abstract DatabaseResult handleQuery(String sql);
 
     /**
      * Subclasses must provide proper data set for SQL statement.
      * @param sql The sql statement to map to a DataSet
      * @return The data set mapped to the given query
      */
-    protected abstract DatabaseResult handleExecute(String sql) throws JdbcServerException;
+    protected abstract DatabaseResult handleExecute(String sql);
 
     /**
      * Subclasses must provide number of row updated by SQL statement.
      * @param sql The sql statement to map to a DataSet
      * @return The amount of rows affected by the sql
      */
-    protected abstract int handleUpdate(String sql) throws JdbcServerException;
+    protected abstract int handleUpdate(String sql);
 
     @Override
-    public void openConnection(final Map<String, String> properties) throws JdbcServerException {
-        log.info("OPEN CONNECTION with properties: " + properties.entrySet()
-                .stream()
-                .map(entry -> entry.getKey() + "=" + entry.getValue())
-                .collect(Collectors.joining(" | ")));
+    public void openConnection(final Map<String, String> properties){
+        if(log.isDebugEnabled()){
+            final String propertiesAsString = properties.entrySet()
+                    .stream()
+                    .map(entry -> entry.getKey() + "=" + entry.getValue())
+                    .collect(Collectors.joining(" | "));
+
+            log.debug("OPEN CONNECTION with properties: {}", propertiesAsString);
+        }
     }
 
     @Override
-    public void createStatement() throws JdbcServerException {
-        log.info("CREATE STATEMENT");
+    public void createStatement(){
+        log.debug("CREATE STATEMENT");
     }
 
     @Override
-    public void closeConnection() throws JdbcServerException {
-        log.info("CLOSE CONNECTION");
+    public void closeConnection(){
+        log.debug("CLOSE CONNECTION");
     }
 
     @Override
-    public void createPreparedStatement(final String sql) throws JdbcServerException {
-        log.info("CREATE PREPARED STATEMENT: " + sql);
+    public void createPreparedStatement(final String sql){
+        log.debug("CREATE PREPARED STATEMENT: {}", sql);
     }
 
     @Override
-    public DatabaseResult executeQuery(final String sql) throws JdbcServerException {
-        log.info("EXECUTE QUERY: " + sql);
+    public DatabaseResult executeQuery(final String sql){
+        log.debug("EXECUTE QUERY: {}", sql);
         final DatabaseResult databaseResult = handleQuery(sql);
 
-        if(log.isDebugEnabled() && databaseResult.isDataSet()){
-            log.debug(String.format("RESULT SET with %s rows", databaseResult.getDataSet().getRows().size()));
-        }
+        logDataSet(databaseResult);
 
-        log.info("QUERY EXECUTION SUCCESSFUL");
+        log.debug("QUERY EXECUTION SUCCESSFUL");
         return databaseResult;
     }
 
     @Override
-    public DatabaseResult executeStatement(final String sql) throws JdbcServerException {
-        log.info("EXECUTE STATEMENT: " + sql);
+    public DatabaseResult executeStatement(final String sql){
+        log.debug("EXECUTE STATEMENT: {}", sql);
+
         final DatabaseResult databaseResult = handleExecute(sql);
 
-        if(log.isDebugEnabled() && databaseResult.isDataSet()){
-            log.debug(String.format("RESULT SET with %s rows", databaseResult.getDataSet().getRows().size()));
-        }
-
-        log.info("STATEMENT EXECUTION SUCCESSFUL");
-
+        logDataSet(databaseResult);
+        log.debug("STATEMENT EXECUTION SUCCESSFUL");
         return databaseResult;
     }
 
     @Override
-    public int executeUpdate(final String sql) throws JdbcServerException {
-        log.info("EXECUTE UPDATE: " + sql);
+    public int executeUpdate(final String sql){
+        log.debug("EXECUTE UPDATE: {}", sql);
 
         final int rows = handleUpdate(sql);
 
-        if(log.isDebugEnabled()){
-            log.debug(String.format("ROWS UPDATED %s", rows));
-        }
-        log.info("UPDATE EXECUTION SUCCESSFUL");
+        log.debug("ROWS UPDATED {}", rows);
+        log.debug("UPDATE EXECUTION SUCCESSFUL");
         return rows;
     }
 
     @Override
-    public void closeStatement() throws JdbcServerException {
-        log.info("CLOSE STATEMENT");
+    public void closeStatement(){
+        log.debug("CLOSE STATEMENT");
     }
 
     @Override
     public void setTransactionState(final boolean transactionState) {
-        log.info(String.format("RECEIVED TRANSACTION STATE CHANGE: %s", String.valueOf(transactionState)));
+        log.debug("RECEIVED TRANSACTION STATE CHANGE: {}", transactionState);
         this.transactionState = transactionState;
     }
 
     @Override
     public boolean getTransactionState() {
-        log.info(String.format("GET TRANSACTION STATE: %s", String.valueOf(transactionState)));
+        log.debug("GET TRANSACTION STATE: {}", transactionState);
         return this.transactionState;
     }
 
     @Override
     public void commitStatements() {
-        log.info("COMMIT STATEMENTS");
+        log.debug("COMMIT STATEMENTS");
     }
 
     @Override
     public void rollbackStatements() {
-        log.info("ROLLBACK STATEMENTS");
+        log.debug("ROLLBACK STATEMENTS");
     }
 
     @Override
     public void createCallableStatement(final String sql){
-        log.info("CREATE CALLABLE STATEMENT: " + sql);
+        log.debug("CREATE CALLABLE STATEMENT: {}", sql);
+    }
+
+    private void logDataSet(final DatabaseResult databaseResult) {
+        if (databaseResult.isDataSet()) {
+            log.debug("RESULT SET with {} rows", databaseResult.getDataSet().getRows().size());
+        }
     }
 }

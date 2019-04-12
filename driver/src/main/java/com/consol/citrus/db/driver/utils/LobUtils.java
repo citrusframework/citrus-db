dd2d28db335e94930d04cc6cc56710ef85dafa31
Sven Hettwer <sven.hettwer@consol.de>
2019-04-12 15:17:54 +0200

    (citrusframework/citrus-db#28) Added common create Blob functionality

--- driver/src/main/java/com/consol/citrus/db/driver/utils/LobUtils.java
+++ driver/src/main/java/com/consol/citrus/db/driver/utils/LobUtils.java
@@ -1,9 +1,12 @@
 package com.consol.citrus.db.driver.utils;
 
+import com.consol.citrus.db.driver.data.CitrusBlob;
 import com.consol.citrus.db.driver.data.CitrusClob;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.ArrayUtils;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.Reader;
 import java.sql.SQLException;
 
@@ -29,8 +32,33 @@ public class LobUtils {
         }
     }
 
+    /**
+     * Creates a {@link CitrusBlob} from the given {@link java.io.InputStream}
+     * @param inputStream The inputStream to create the clob from
+     * @param length The length of the data to take from the stream or -1 to transfer the whole content
+     * @return The created Citrus Blob
+     * @throws SQLException In case the blob could not be created from the reader
+     */
+    public CitrusBlob createBlobFromInputStream(final InputStream inputStream, final int length) throws SQLException {
+        try{
+            return createBlob(length, IOUtils.toByteArray(inputStream));
+        } catch (final IOException e) {
+            throw new SQLException("Could not create Clob from reader", e);
+        }
+    }
+
     public long applyOffset(final long pos) {
         return pos - 1;
+    }
+
+    private CitrusBlob createBlob(final int length, final byte[] desiredBlobContent) {
+        final CitrusBlob citrusBlob = new CitrusBlob();
+        if(length >= 0){
+            citrusBlob.setBytes(1, ArrayUtils.subarray(desiredBlobContent, 0, length));
+        }else{
+            citrusBlob.setBytes(1, desiredBlobContent);
+        }
+        return citrusBlob;
     }
 
     private CitrusClob createClob(final int length, final String desiredClobContent) {
--- driver/src/test/java/com/consol/citrus/db/driver/utils/LobUtilsTest.java
+++ driver/src/test/java/com/consol/citrus/db/driver/utils/LobUtilsTest.java
@@ -1,10 +1,14 @@
 package com.consol.citrus.db.driver.utils;
 
+import com.consol.citrus.db.driver.data.CitrusBlob;
 import com.consol.citrus.db.driver.data.CitrusClob;
 import org.apache.commons.io.IOUtils;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.StringReader;
+import java.sql.SQLException;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
@@ -71,7 +75,7 @@ public class LobUtilsTest {
     }
 
     @Test
-    public void testApplyOffset() throws Exception {
+    public void testApplyOffset() {
 
         //GIVEN
         final long position = 1;
@@ -82,5 +86,35 @@ public class LobUtilsTest {
 
         //THEN
         assertEquals(positionWithOffset, expectedPosition);
+    }
+
+    @Test
+    public void testSetUnlimitedBlobFromReader() throws IOException, SQLException {
+
+        //GIVEN
+        final String expectedClobContent = "Stay positive, always!";
+        final InputStream inputStream = IOUtils.toInputStream(expectedClobContent, "UTF8");
+
+        //WHEN
+        final CitrusBlob clobFromReader = lobUtils.createBlobFromInputStream(inputStream, -1);
+
+        //THEN
+        final String clobContent = IOUtils.toString(clobFromReader.getBinaryStream(), "UTF8");
+        assertEquals(clobContent, expectedClobContent);
+    }
+
+    @Test
+    public void testSetLimitedBlobFromReader() throws Exception {
+
+        //GIVEN
+        final InputStream inputStream = IOUtils.toInputStream("Stay positive, always!", "UTF8");
+        final String expectedClobContent = "Stay positive";
+
+        //WHEN
+        final CitrusBlob blobFromInputStream = lobUtils.createBlobFromInputStream(inputStream, 13);
+
+        //THEN
+        final String clobContent = IOUtils.toString(blobFromInputStream.getBinaryStream(), "UTF8");
+        assertEquals(clobContent, expectedClobContent);
     }
 }

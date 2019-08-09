85a92bd43839ad1867146718d80c8e13b8cca0fe
Sven Hettwer <sven.hettwer@consol.de>
2019-08-09 14:23:13 +0200

    (citrusframework/citrus-db#29) Compensate to many getNextRow calls on DataSets

--- driver/src/test/java/com/consol/citrus/db/driver/dataset/DataSetTest.java
+++ driver/src/test/java/com/consol/citrus/db/driver/dataset/DataSetTest.java
@@ -4,22 +4,52 @@ import com.consol.citrus.db.driver.data.Row;
 import com.jparams.verifier.tostring.ToStringVerifier;
 import nl.jqno.equalsverifier.EqualsVerifier;
 import org.testng.annotations.Test;
+import org.testng.collections.Lists;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNull;
 
 public class DataSetTest {
 
+    private final DataSet dataSet = new DataSet();
+
     @Test
     public void testEmptyDataSetReturnsNullOnGetNextRow() {
-
-        //GIVEN
-        final DataSet dataSet = new DataSet();
 
         //WHEN
         final Row nextRow = dataSet.getNextRow();
 
         //THEN
         assertNull(nextRow);
+    }
+
+    @Test
+    public void testCursorPositionIsPreservationInCaseOfNoData(){
+
+        //WHEN
+        dataSet.getNextRow();
+
+        //THEN
+        assertEquals(dataSet.getCursor(), 0);
+    }
+
+    @Test
+    public void testCursorPositionMovesInCaseOfMoreData(){
+
+        //GIVEN
+        final Map<String, Object> testData = Collections.singletonMap("foo","bar");
+        final List<Row> testDataRows = Lists.newArrayList(new Row(testData));
+        final DataSet dataSet = new DataSet(testDataRows);
+
+        //WHEN
+        dataSet.getNextRow();
+
+        //THEN
+        assertEquals(dataSet.getCursor(), 1);
     }
 
     @Test
